67fd574455f66abaad75c404b6b827c1b58d6838
amakash-warc <157267081+amakash-warc@users.noreply.github.com>
2024-04-04 16:52:45 +0600

    added server

---
+++ app.js
@@ -0,0 +1,58 @@
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = 4006
+
+const { exec } = require("child_process");
+
+app.use(cors());
+app.use(express.json());
+
+app.get('/', (req, res) => {
+  res.send('Test GIT Deploy')
+})
+
+app.post('/git/webhooks/:appname',(req, res)=>{
+   const data =  req.body;
+   const appname = req.params['appname'];
+
+   if(appname =='test-git-deploy'){
+     RunActions();
+   }
+
+   return res.json('ok');
+})
+
+
+function RunActions(){
+    const commands = ['git pull',
+    'git status',
+    'git submodule sync',
+    'git submodule update',
+    'git submodule status',
+    'rm -rfv ~/Desktop/target/',
+    'cp ./index.html ~/Desktop/target/'];
+
+
+    commands.forEach((value)=>{
+        execCommand(value);
+    })
+}
+
+function execCommand(cmd){
+    exec(cmd, (error, stdout, stderr) => {
+        if (error) {
+            console.log(`error: ${error.message}`);
+            return;
+        }
+        if (stderr) {
+            console.log(`stderr: ${stderr}`);
+            return;
+        }
+        console.log(`stdout: ${stdout}`);
+    });
+}
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
